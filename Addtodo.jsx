69a44f7ac84da1f540165a0ec9aa2fb1315280e3
Rakshit <55257622+rakshit-goel-cse@users.noreply.github.com>
2024-03-09 16:41:34 +0530

    when adding title if title is null at focus and show msg

--- Addtodo.jsx
+++ Addtodo.jsx
@@ -1,35 +1,54 @@
 import { Text, TouchableOpacity } from "react-native";
 import { s } from "./Addtodo-style";
 import Dialog from "react-native-dialog";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export const Addtodo = ({ addTodo }) => {
   const [showDia, setShowDia] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [titleNull, setTitleNull] = useState(false);
+  const ref = useRef(null);
+
+  const addTitle = (temp) => {
+    console.log(temp === " " && title === "");
+    temp === " " && title === "" ? "" : setTitle(temp);
+  };
+
+  const pressAdd = () => {
+    title !== ""
+      ? (addTodo(title, description),
+        setTitle(""),
+        setDescription(""),
+        setShowDia(false))
+      : (setTitleNull(true), ref.current.focus());
+  };
+
   return (
     <>
       <Dialog.Container style={s.dialog} visible={showDia}>
         <Dialog.Title>Add Todo</Dialog.Title>
         <Dialog.Description>Add Details to add new todo.</Dialog.Description>
+
         <Dialog.Input
+          textInputRef={ref}
           label="Title"
-          onChange={(event) => setTitle(event.nativeEvent.text)}
+          value={title}
+          onChange={(event) => addTitle(event.nativeEvent.text)}
         />
+
+        {titleNull && (
+          <Dialog.Description style={{ color: "red", fontSize: 8 }}>
+            Title can not be empty
+          </Dialog.Description>
+        )}
+
         <Dialog.Input
           label="Description"
           onChange={(temp) => setDescription(temp.nativeEvent.text)}
         />
         <Dialog.Button label="Cancel" onPress={() => setShowDia(false)} />
-        <Dialog.Button
-          label="Add"
-          onPress={() => {
-            title !== "" ? addTodo(title, description) : "";
-            setTitle("");
-            setDescription("");
-            setShowDia(false);
-          }}
-        />
+        <Dialog.Button label="Add" onPress={() => pressAdd()} />
       </Dialog.Container>
 
       <TouchableOpacity style={s.btn} onPress={() => setShowDia(true)}>
