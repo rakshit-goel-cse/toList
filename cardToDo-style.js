f2bfc816ab043e6780208fccc299168958237aa2
Rakshit <55257622+rakshit-goel-cse@users.noreply.github.com>
2024-03-11 03:09:10 +0530

    Added swip gesture to mark complete or delete

--- cardToDo-style.js
+++ cardToDo-style.js
@@ -1,29 +1,37 @@
 import { StyleSheet } from "react-native";
 
-export const s=StyleSheet.create({
-    touch:{
-        height:80,
-        flexDirection:"row",
-        alignItems:"center",
-        justifyContent:"space-between",
-        backgroundColor: "white",
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        paddingHorizontal:10,
-        borderRadius:50,
-        marginVertical:3,      
+export const s = StyleSheet.create({
+  touch: {
+    height: 80,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    click:{
-        height:30,
-        width:30,
-    },
-    text:{
-        fontSize:20
-    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    paddingHorizontal: 10,
+    borderRadius: 50,
+    marginVertical: 3,
+  },
+  click: {
+    height: 30,
+    width: 30,
+  },
+  text: {
+    fontSize: 20,
+  },
+  leftContent: {
+    height: 60,
+    width: 60,
+    alignSelf: "flex-end",
+    marginRight: 10,
+    marginTop: 10,
+    backgroundColor: "#F6F6F6",
+  },
 });
--- cardToDo.jsx
+++ cardToDo.jsx
@@ -1,6 +1,10 @@
 import { Image, Text, TouchableOpacity, Alert } from "react-native";
 import click from "./assets/check.png";
+import deleteImg from "./assets/delete.png";
+import undoImg from "./assets/undo.png";
 import { s } from "./cardToDo-style";
+import Swipeable from 'react-native-swipeable';
+
 export function CardToDo({ todo, updateToDo, deleteToDo }) {
   const deleteTodoAlert = () => {
     Alert.alert(
@@ -34,7 +38,14 @@ export function CardToDo({ todo, updateToDo, deleteToDo }) {
     );
   };
 
+  const leftContent=<Image source={todo.isDone? undoImg : click} style={s.leftContent} />
+
+  const rightContent=<Image source={deleteImg} style={[s.leftContent,{alignSelf:'flex-start'}]} />
+
   return (
+    <Swipeable leftContent={leftContent} rightContent={rightContent}
+    onLeftActionRelease={()=>updateToDo({ ...todo, isDone: !todo.isDone })}
+    onRightActionRelease={deleteTodoAlert}>
     <TouchableOpacity
       style={s.touch}
       onPress={() => {
@@ -42,12 +53,15 @@ export function CardToDo({ todo, updateToDo, deleteToDo }) {
       }}
       onLongPress={deleteTodoAlert}
     >
+      
       <Text
         style={[s.text, todo.isDone && { textDecorationLine: "line-through" }]}
       >
         {todo.title}
       </Text>
       {todo.isDone && <Image source={click} style={s.click} />}
+      
     </TouchableOpacity>
+    </Swipeable>
   );
 }
